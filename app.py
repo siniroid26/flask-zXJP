805570698bf9ecbdf1f6bcdb2cc77221be568531
Aleksandr Nikolchenko <75898267+siniroid26@users.noreply.github.com>
2023-09-21 00:10:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
